6b18636a6ab44cb99528a556fb0c5461d08ea123
ajsalmi <a.johannes.salmi@gmail.com>
2019-09-18 10:15:55 +0300

    esittelyversio

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,17 @@
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    
+
     public static void main(String[] args) {
         ArrayList<Integer> pelaaja = new ArrayList<>();
         ArrayList<Integer> emanta = new ArrayList<>();
         Pakka pakka = new Pakka();
         Scanner lukija = new Scanner(System.in);
-        
+
         System.out.println("Tervetuloa pelaamaan Venttiä!\n\nPelin säännöt");
-      
+
         System.out.println("Tarkoituksena on saada korttien yhteissummaksi 21 tai mahdollisimman lähelle sitä.");
         System.out.println("Lähemmäksi 21 päässyt osanottaja voittaa.");
         System.out.println("Mikäli pelaajan tai emännän korttien summa ylittää 21, häviää summan ylittänyt taho automaattisesti.");
@@ -24,105 +25,99 @@ public class Main {
         System.out.println("Peli päättyy emännän lopettaessa vuoronsa.");
         System.out.println("Onnea matkaan!");
         System.out.println("");
-        
+
         //Pelaajan pelivuoro
         System.out.println("Pelivuorossa pelaaja:");
         pelaaja.add(pakka.nostaKortti());
         System.out.println("Ensimmäinen kortti on: " + pelaaja.get(0));
-        int indeksi = 1;
-        
-        while (true) {
+
+        while (true) { // pelaajan pelivuoro
             System.out.println("Nostetaanko uusi kortti? [kyllä/ei]");
             String vastaus = lukija.nextLine();
-            
-            
-                System.out.println("Kortti on: " + pelaaja.get(indeksi)); 
-                indeksi++;
+
+            if (vastaus.equals("kyllä")) {
+                int uusiKortti = pakka.nostaKortti();
+                pelaaja.add(uusiKortti);
+                System.out.println("\nKortti on: " + uusiKortti);
+            } else if (vastaus.equals("ei")) {
+                break;
             } else {
-                System.out.println("\nVirheellinen syöte!\n");
+                System.out.println("\nvirheellinen syöte!\n");
                 continue;
-            }      
-            
-            if (havisiko(pelaaja) == true) { //jos käsi ylittää 21, tarkistetaan onko ässiä
+            }
+
+            if (havisiko(pelaaja)) { //jos käsi ylittää 21, tarkistetaan onko ässiä
                 assanMuunto(pelaaja);
             }
 
-            if (!havisiko(pelaaja)&& pelaaja.size() == 5) {
-                System.out.println("Viisi korttia muodostaa Ventin!");
-                System.out.println("Korttien summa on: 21");
-                pelaaja.clear();
-                pelaaja.add(21);
-                break;   
-            }
-            System.out.println("Korttien summa on: " + laskeSumma(pelaaja));
-            
             if (havisiko(pelaaja)) { //tarkistetaan häviääkö pelaaja suoraan
+                System.out.println("Korttien summa on: " + laskeSumma(pelaaja));
                 System.out.println("\nPelaaja häviää.\nEmäntä voittaa!");
                 break;
-            }            
+            } else if ( pelaaja.size() == 5) {
+                System.out.println("Viisi korttia muodostaa Ventin!\n\"Korttien summa on: 21");
+                pelaaja.clear();
+                pelaaja.add(21);
+                break;
+            }
+            System.out.println("Korttien summa on: " + laskeSumma(pelaaja));
+
         }
-        
+
         //emännän pelivuoro
         if (!havisiko(pelaaja)) {
-            
+
             System.out.println("\nPelivuorossa emäntä:");
             emanta.add(pakka.nostaKortti());
             assanMuunto(emanta);
             System.out.println("Ensimmäinen kortti on: " + emanta.get(0));
-            int tokaIndeksi = 1;
-            
+
             while (true) {
-                if (laskeSumma(emanta)>= laskeSumma(pelaaja)|| laskeSumma(emanta) >=20) {
-                    System.out.println("");
-                    System.out.println("Pelaaja häviää.");
-                    System.out.println("Emäntä voittaa!");
+                if (laskeSumma(emanta) >= laskeSumma(pelaaja)
+                        || laskeSumma(emanta) == 20
+                        || laskeSumma(emanta) == 21) {
+                    System.out.println("\nPelaaja häviää.\nEmäntä voittaa!");
                     break;
                 } else {
-                    emanta.add(pakka.nostaKortti());
+                    int kortti = pakka.nostaKortti();
+                    System.out.println("\nSeuraava kortti on: " + kortti);
+                    emanta.add(kortti);
                     assanMuunto(emanta);
-                    System.out.println("Seuraava kortti on: " + emanta.get(tokaIndeksi));
-                    tokaIndeksi++;
                     System.out.println("Korttien summa on: " + laskeSumma(emanta));
-                    
-                    if (havisiko(emanta) == true) {
+
+                    if (havisiko(emanta)) {
                         System.out.println("");
                         System.out.println("Emäntä häviää.");
                         System.out.println("Pelaaja voittaa!");
                         break;
                     }
                 }
-                emanta.add(pakka.nostaKortti());
-                System.out.println("Seuraava kortti on: " + emanta.get(tokaIndeksi));
-                tokaIndeksi++;
-                assanMuunto(emanta);
-                System.out.println("Korttien summa on: " + laskeSumma(emanta));
-                    
+
                 if (havisiko(emanta)) {
                     System.out.println("\nEmäntä häviää.\nPelaaja voittaa!");
                     break;
-                }          
+                }
             }
-        }    
+        }
     }
-    
+
     //metodit
     public static Integer laskeSumma(ArrayList<Integer> lista) {
-        int summa = 0;     
+        int summa = 0;
         for (Integer arvo : lista) {
-                summa = summa + arvo;
-        } 
-        return summa;        
+            summa = summa + arvo;
+        }
+        return summa;
     }
-    
+
     public static void assanMuunto(ArrayList<Integer> lista) { //tarkistaa löytyykö ässä ja muuttaa sen 1
         if (lista.contains(14)) {
             lista.remove(Integer.valueOf(14));
             lista.add(1);
         }
     }
-    public static boolean havisiko(ArrayList<Integer> henkilo) { 
-        return (laskeSumma(henkilo)>21);
+
+    public static boolean havisiko(ArrayList<Integer> henkilo) {
+        return (laskeSumma(henkilo) > 21);
     }
-    
-    
 }
